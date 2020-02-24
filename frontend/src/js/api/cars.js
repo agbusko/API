f51e7b0fefb1797f6142775d8c1f94d85bc702ea
Artem <agbusko@gmail.com>
2020-02-24 23:01:09 +0300

    AB: пример axios

--- frontend/src/js/api/cars.js
+++ frontend/src/js/api/cars.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export async function all(){
     let data = await makeRequest('https://ragnar2019.firebaseio.com/data.json?auth=2wUlcDoifwuTy8NjhflVJF6BhDFMVpcom342PVHl');
     return data;
@@ -6,7 +8,7 @@ export async function all(){
 export async function add(car){
     let data = await makeRequest('https://ragnar2019.firebaseio.com/data.json?auth=2wUlcDoifwuTy8NjhflVJF6BhDFMVpcom342PVHl', {
         method: 'POST',
-        body: JSON.stringify(car)
+        data: JSON.stringify(car) // data - для axios, body - для fetch
     });
 
     return data;
@@ -14,14 +16,23 @@ export async function add(car){
 
 //  AB: общий функционал для запросов;
 function makeRequest(url, options = {}){
-    return fetch(url, options).then((response) => {
-        if (response.status !== 200){
-            return response.text().then((text) => {
-                throw new Error(text);
-            });
-        }
-
-        return response.json();
+    return axios(url, options)                 // axios.get() - по умолчанию;
+        .then((response) => {
+        return response.data;
     });
 }
+
+// Пример Fetch;
+
+// function makeRequest(url, options = {}){
+//     return fetch(url, options).then((response) => {
+//         if (response.status !== 200){
+//             return response.text().then((text) => {
+//                 throw new Error(text);
+//             });
+//         }
+//
+//         return response.json();
+//     });
+// }
 
